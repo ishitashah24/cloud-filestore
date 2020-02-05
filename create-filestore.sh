2e1074200c23887becbade130af885e5b423c403
ishitashah24 <60626073+ishitashah24@users.noreply.github.com>
2020-02-05 13:45:15 -0600

    Update create-filestore.sh

--- create-filestore.sh
+++ create-filestore.sh
@@ -1,8 +1,14 @@
-#command to create cloud filestore instance
+#!/usr/bin/env bash
 
+export PROJECT_ID = 
+export ZONE = us-west1-a
+export FILESHARE_NAME = "test_filestore"
+export CAPACITY = 1TB
+
+#command to create cloud filestore instance
 gcloud filestore instances create nfs-server \
-    --project=[PROJECT_ID] \
-    --zone=us-central1-c \
+    --project=$PROJECT_ID \
+    --zone=$ZONE \
     --tier=STANDARD \
-    --file-share=name="vol1",capacity=1TB \
+    --file-share=name=${FILESHARE_NAME},capacity=${CAPACITY} \
     --network=name="default"
