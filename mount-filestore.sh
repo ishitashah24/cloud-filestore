2afcb13d86ea270b41876b43a8c211640aa8b097
ishitashah24 <60626073+ishitashah24@users.noreply.github.com>
2020-02-05 12:56:45 -0600

    added functions

--- mount-filestore.sh
+++ mount-filestore.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
 #install nfs on Ubuntu
-sudo apt-get -y update
-sudo apt-get install nfs-common
+function install_nfs()
+{
+  apt-get -y update
+  apt-get install nfs-common
+}
 
-#make local directory to map to cloud filestore file share
-sudo mkdir -p mount-point-directory
-
-#set permissions to allow users to read/write
-sudo chmod go+rw mount-point-directory
-
-#confirm that the filestore file share is mounted
-df -h --type=nfs
+function set_mount_directory()
+{
+  export mount_point_directory = /mnt/test
+  export IP_ADDRESS = 
+  export FILESTORE_INSTANCE = 
+  
+  #make local directory to map to cloud filestore file share
+  mkdir -p ${mount_point_directory}  
+  
+  #mount the filestore instance
+  mount ${IP_ADDRESS}:/${FILESTORE_INSTANCE} ${mount_point_directory}
+  
+  #set permissions to allow users to read/write
+  chmod go+rw ${mount_point_directory}
+  
+  #confirm that the filestore file share is mounted
+  df -h --type=nfs
+}
 
