e4c9872988f392529c247647403c219e1ebd1b92
ishitashah24 <60626073+ishitashah24@users.noreply.github.com>
2020-02-05 16:22:38 -0600

    Update create-dataproc.sh

--- create-dataproc.sh
+++ create-dataproc.sh
@@ -1,4 +1,5 @@
-BUCKET=<your_init_actions_bucket>
+#create a bucket for init actions
+BUCKET=<init_actions_bucket>
 CLUSTER=<cluster_name>
-gsutil cp cloud-firestore/mount-filestore.sh gs://${BUCKET}/
+gsutil cp cloud-filestore/mount-filestore.sh gs://${BUCKET}/
 gcloud dataproc clusters create ${CLUSTER} --initialization-actions gs://${BUCKET}/mount-filestore.sh
