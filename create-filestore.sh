65215aec1cdb61f984a8bd8004c15e77cba5b7d1
ishitashah24 <60626073+ishitashah24@users.noreply.github.com>
2020-02-05 14:14:22 -0600

    Update create-filestore.sh

--- create-filestore.sh
+++ create-filestore.sh
@@ -5,10 +5,15 @@ export ZONE = us-west1-a
 export FILESHARE_NAME = "test_filestore"
 export CAPACITY = 1TB
 
-#command to create cloud filestore instance
+#create cloud filestore instance
 gcloud filestore instances create nfs-server \
-    --project=$PROJECT_ID \
-    --zone=$ZONE \
+    --project=${PROJECT_ID} \
+    --zone=${ZONE} \
     --tier=STANDARD \
     --file-share=name=${FILESHARE_NAME},capacity=${CAPACITY} \
     --network=name="default"
+
+#get ip address of the instance
+gcloud filestore instances describe ${FILESHARE_NAME} \
+    --project=${PROJECT_ID} \
+    --zone=${ZONE}
