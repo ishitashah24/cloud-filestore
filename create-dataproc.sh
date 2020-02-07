24a935c6f95ef52f4288809027597f35bf416b65
ishitashah24 <60626073+ishitashah24@users.noreply.github.com>
2020-02-07 16:00:44 -0600

    Delete create-dataproc.sh

--- create-dataproc.sh
+++
@@ -1,5 +0,0 @@
-#create a bucket for init actions
-BUCKET=<init_actions_bucket>
-CLUSTER=<cluster_name>
-gsutil cp cloud-filestore/mount-filestore.sh gs://${BUCKET}/
-gcloud dataproc clusters create ${CLUSTER} --initialization-actions gs://${BUCKET}/mount-filestore.sh
